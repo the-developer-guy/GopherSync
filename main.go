1c6bdbc085250cee459d7a63c24cc82955cbedc7
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2025-03-10 22:07:57 +0100

    Added basic duplicate finder

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 )
+
+type ArchiveFile struct {
+	Path string `json:",string"`
+	Hash string `json:",string"`
+}
 
 func main() {
 	fmt.Println("Deduplicating files")
@@ -14,11 +20,23 @@ func main() {
 		panic("Please provide at least the folder you want to deduplicate!")
 	}
 
-	collectFiles(os.Args[1])
+	uniqueFiles, allFiles, duplicates := collectFiles(os.Args[1])
+	fmt.Printf("%d unique files and %d files in root path\n", len(uniqueFiles), len(allFiles))
+
+	duplicatesJson, err := json.Marshal(duplicates)
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("duplicates.json", duplicatesJson, 0666)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func collectFiles(path string) map[string]string {
-	files := map[string]string{}
+func collectFiles(path string) (map[string]string, []ArchiveFile, []ArchiveFile) {
+	uniqueFiles := map[string]string{}
+	files := []ArchiveFile{}
+	duplicates := []ArchiveFile{}
 
 	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -30,12 +48,25 @@ func collectFiles(path string) map[string]string {
 			if err != nil {
 				return err
 			}
-			files[h] = path
+
+			af := ArchiveFile{
+				Path: path,
+				Hash: h,
+			}
+			files = append(files, af)
+
+			_, ok := uniqueFiles[h]
+			if ok {
+				duplicates = append(duplicates, af)
+			} else {
+				uniqueFiles[h] = path
+			}
+
 		}
 		return nil
 	})
 
-	return files
+	return uniqueFiles, files, duplicates
 }
 
 func hashFile(path string) (string, error) {
