fd04bb3efb9da724a7538fb88fece2d3c7a95e73
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2025-03-11 21:40:42 +0100

    Added statefile handling

--- files.go
+++ files.go
@@ -15,6 +15,35 @@ type ArchiveFile struct {
 	Hash string `json:",string"`
 }
 
+func LoadStatefile(path string) (map[string]string, error) {
+	statefile, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	archivedFiles := map[string]string{}
+	err = json.Unmarshal(statefile, &archivedFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	return archivedFiles, nil
+}
+
+func StoreStatefile(path string, archivedFiles map[string]string) error {
+	statefileJson, err := json.Marshal(archivedFiles)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(path, statefileJson, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func Backup(sourceRoot, destinationRoot string, archivedFiles map[string]string) {
 
 	filesToArchive := []ArchiveFile{}
